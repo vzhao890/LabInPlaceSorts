b5f24325d4e957a2306850e6432ba8f478585dec
BT_1N3_31 <vzhao2047@bths.edu>
2018-11-16 10:17:14 -0500

    Initial commit

---
+++ src/InPlaceSorts.java
@@ -0,0 +1,16 @@
+public class InPlaceSorts
+{
+    public static void insertionSort(int[] list1)
+    {
+
+    }
+    public static void selectionSort(double[] list1)
+    {
+
+    }
+    public static void bubbleSort(String[] list1)
+    {
+
+    }
+
+}
---
+++ src/Runner.java
@@ -0,0 +1,7 @@
+public class Runner
+{
+    public static void main(String[] args)
+    {
+        int[]list1={1,2,3,3,3,3,3,54,4,5,5,1};
+    }
+}
