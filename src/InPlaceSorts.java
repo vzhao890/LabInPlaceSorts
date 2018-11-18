c14970bf0b408626062ad1fbd034c39cd89febe5
User <vzhao2047@bths.edu>
2018-11-18 01:35:30 -0500

    3

--- src/InPlaceSorts.java
+++ src/InPlaceSorts.java
@@ -3,15 +3,8 @@ public class InPlaceSorts
 
     public static void insertionSort(int[] list1)
     {
-        int k;
-        for (int i = 1; i < list1.length; i++)
-        {
-            int currentElement = list1[i];
-            for (k = i - 1; k >= 0 && list1[k] > currentElement; k--) {
-                list1[k + 1] = list1[k];
-            }
-            list1[k + 1] = currentElement;
-        }
+
+    
 
     }
     public static void selectionSort(double[] list3)
@@ -19,12 +12,14 @@ public class InPlaceSorts
         double fillA;
         for(int i=0;i<list3.length-1;i++)
         {
-            if(list3[i]>list3[i+1])
+            for(int k=i+1;k<list3.length;k++)
             {
-                fillA=list3[i];
-                list3[i]=list3[i+1];
-                list3[i+1]=fillA;
-
+                if(list3[i]>=list3[k])
+                {
+                    fillA=list3[i];
+                    list3[i]=list3[k];
+                    list3[k]=fillA;
+                }
             }
 
         }
--- src/Runner.java
+++ src/Runner.java
@@ -3,7 +3,17 @@ public class Runner
     public static void main(String[] args) {
         int[] list1 = {1, 2, 3, 3, 3, 3, 3, 54, 4, 5, 5, 1};
         String[] list2 = {"a","b","a"};
-        double[] list3 = {0.0,0.1,0.0,0.2};
+        double[] list3 = {0.3,0.1,0.0,0.2};
+        InPlaceSorts.selectionSort(list3);
+        for(int i=0;i<list3.length;i++)
+        {
+            System.out.print(" "+list3[i]);
+        }
+        System.out.println();
+        for(int i=0;i<list1.length;i++)
+        {
+            System.out.print(" "+list1[i]);
+        }
     }
 
 }
