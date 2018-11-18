06979e1c25411ed00200783fab5582fcc46cd12e
User <vzhao2047@bths.edu>
2018-11-18 02:02:20 -0500

    3

--- src/InPlaceSorts.java
+++ src/InPlaceSorts.java
@@ -3,8 +3,29 @@ public class InPlaceSorts
 
     public static void insertionSort(int[] list1)
     {
+        int[] sorted= new int[list1.length];
+        int a= list1.length-1;
+        int b=1;
+        int hold;
+        list1[a]=list1[0];
 
-    
+        while(a>0)
+        {
+            if(list1[b]>=sorted[a])
+            {
+                hold=sorted[a];
+                sorted[a]=list1[b];
+                a=a-1;
+                b += 1;
+                sorted[a]=hold;
+
+            }
+
+        }
+        for(int i=0;i<list1.length-1;i++)
+        {
+            list1[i]=sorted[i];
+        }
 
     }
     public static void selectionSort(double[] list3)
--- src/Runner.java
+++ src/Runner.java
@@ -1,10 +1,11 @@
 public class Runner
 {
     public static void main(String[] args) {
-        int[] list1 = {1, 2, 3, 3, 3, 3, 3, 54, 4, 5, 5, 1};
+        int[] list1 = {1, 2,3, 54, 4, 5, 5, 1};
         String[] list2 = {"a","b","a"};
-        double[] list3 = {0.3,0.1,0.0,0.2};
+        double[] list3 = {0.3,0.1,0.0,0.2,0.4,0.1};
         InPlaceSorts.selectionSort(list3);
+        InPlaceSorts.insertionSort(list1);
         for(int i=0;i<list3.length;i++)
         {
             System.out.print(" "+list3[i]);
