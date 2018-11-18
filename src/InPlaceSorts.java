44371f12b717408e58a1fcad06b7b82801a9157b
User <vzhao2047@bths.edu>
2018-11-18 01:12:50 -0500

    3

--- src/InPlaceSorts.java
+++ src/InPlaceSorts.java
@@ -1,12 +1,33 @@
 public class InPlaceSorts
 {
+
     public static void insertionSort(int[] list1)
     {
+        int k;
+        for (int i = 1; i < list1.length; i++)
+        {
+            int currentElement = list1[i];
+            for (k = i - 1; k >= 0 && list1[k] > currentElement; k--) {
+                list1[k + 1] = list1[k];
+            }
+            list1[k + 1] = currentElement;
+        }
 
     }
-    public static void selectionSort(double[] list1)
+    public static void selectionSort(double[] list3)
     {
+        double fillA;
+        for(int i=0;i<list3.length-1;i++)
+        {
+            if(list3[i]>list3[i+1])
+            {
+                fillA=list3[i];
+                list3[i]=list3[i+1];
+                list3[i+1]=fillA;
 
+            }
+
+        }
     }
     public static void bubbleSort(String[] list1)
     {
--- src/Runner.java
+++ src/Runner.java
@@ -1,7 +1,9 @@
 public class Runner
 {
-    public static void main(String[] args)
-    {
-        int[]list1={1,2,3,3,3,3,3,54,4,5,5,1};
+    public static void main(String[] args) {
+        int[] list1 = {1, 2, 3, 3, 3, 3, 3, 54, 4, 5, 5, 1};
+        String[] list2 = {"a","b","a"};
+        double[] list3 = {0.0,0.1,0.0,0.2};
     }
+
 }
